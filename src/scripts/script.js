74aee571c84a0b185f43c716675b9f539415f9a0
ArefAH <arefabouhamdan@gmail.com>
2024-11-02 20:41:20 +0200

    Added load more feature

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,11 +1,15 @@
 const instance = axios.create({
   baseURL: "https://www.world-wonders-api.org/v0/",
 });
+let currentNumber = 0;
+let postsNumber = 10;
 
 async function fetchPosts() {
   const response = await instance.get("/wonders");
-  const listOfPosts = response.data;
-
+  const listOfPosts = response.data.slice(
+    `${currentNumber}`,
+    `${currentNumber + postsNumber}`
+  );
   for (let post of listOfPosts) {
     const postElement = document.importNode(postTemplate.content, true);
     console.log(postElement);
@@ -13,6 +17,13 @@ async function fetchPosts() {
     postElement.querySelector("h2").textContent = post.name.toUpperCase();
     postList.appendChild(postElement);
   }
+  currentNumber += postsNumber;
+
+  if (currentNumber >= response.data.length) {
+    more.style.display = "none";
+  }
 }
+
+more.addEventListener("click", fetchPosts);
 
 fetchPosts();
