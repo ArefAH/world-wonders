ab7c39fcbd79bddd414da1dfee82c0bca6f231f9
ArefAH <arefabouhamdan@gmail.com>
2024-11-03 18:12:28 +0200

    Added search function and fixed responsiveness

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,5 +1,7 @@
 const wonders = document.getElementById("wonders")
 const postTemplate = document.getElementById("single-post");
-const postList = document.querySelectorAll("post");
 const more = document.getElementById("load");
+const search = document.getElementById("search");
+
+
 
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,13 +4,32 @@ const instance = axios.create({
 let currentNumber = 0;
 let postsNumber = 12;
 
+
+
+const searchPosts = () => {
+  const postList = document.querySelectorAll(".post");
+  let searchValue = search.value.toLowerCase();
+  postList.forEach((post) => {
+    const postContent = post.textContent.toLowerCase();
+    if (postContent.includes(searchValue)) {
+      post.style.display = "block";
+    } else {
+      post.style.display = "none";
+    }
+  });
+};
+
 const createPost = (post) => {
   const postElement = document.importNode(postTemplate.content, true);
   postElement.querySelector("img").src = post.links.images[0];
   postElement.querySelector("h3").textContent = post.name.toUpperCase();
   postElement.querySelector("p").textContent = post.location;
+  postElement.querySelector("button").addEventListener("click", () =>{
+    localStorage.setItem("selectedPost", JSON.stringify(post));
+    window.location.href = "src/pages/information.html"
+  });
   wonders.appendChild(postElement);
-}
+};
 
 async function fetchPosts() {
   const response = await instance.get("/wonders");
@@ -28,5 +47,6 @@ async function fetchPosts() {
 }
 
 more.addEventListener("click", fetchPosts);
+search.addEventListener("input", searchPosts);
 
 fetchPosts();
