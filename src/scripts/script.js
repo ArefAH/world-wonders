193528c83ffcedbba64eea91f9f4862f7bf34f86
ArefAH <arefabouhamdan@gmail.com>
2024-11-03 14:25:14 +0200

    Finished main page layout and design

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,20 +4,22 @@ const instance = axios.create({
 let currentNumber = 0;
 let postsNumber = 12;
 
+const createPost = (post) => {
+  const postElement = document.importNode(postTemplate.content, true);
+  postElement.querySelector("img").src = post.links.images[0];
+  postElement.querySelector("h3").textContent = post.name.toUpperCase();
+  postElement.querySelector("p").textContent = post.location;
+  wonders.appendChild(postElement);
+}
+
 async function fetchPosts() {
   const response = await instance.get("/wonders");
   const listOfPosts = response.data.slice(
     `${currentNumber}`,
     `${currentNumber + postsNumber}`
   );
-  for (let post of listOfPosts) {
-    const postElement = document.importNode(postTemplate.content, true);
-    console.log(postElement);
-    postElement.querySelector("img").src = post.links.images[0];
-    postElement.querySelector("h3").textContent = post.name.toUpperCase();
-    postElement.querySelector("p").textContent = post.location;
-    wonders.appendChild(postElement);
-  }
+  for (let post of listOfPosts) createPost(post);
+
   currentNumber += postsNumber;
 
   if (currentNumber >= response.data.length) {
